d21ac88404186f8812f82da0a610fee43d86b1a4
Evan <raunerevan@gmail.com>
2019-12-31 00:33:48 -0600

    test commit

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 # scripting
-=======
 # bash-scripting
->>>>>>> a9b2bc27bc985d3fa9ec541bf33f32f78d49ed88
--- variables_pass_a.sh
+++ variables_pass_a.sh
@@ -5,11 +5,14 @@ myvar="shell scripting"
 
 echo $myvar
 myvar2="TEST 2"
+myvar3="TEST 3"
 
 echo $myvar2
+echo $myvar3
 
 #if you comment the line below, the variable will not get passed into the next script
 export myvar2
+export myvar3
 
 
 bash /mnt/c/Users/Evan/Desktop/development/bash_scripts/variables_pass_b.sh
