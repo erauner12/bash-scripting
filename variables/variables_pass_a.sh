965298686c87cc9bd1f41f349ad87c613789318c
Evan Rauner <raunerevan@gmail.com>
2019-12-31 01:10:55 -0600

    added another variable

--- variables/variables_pass_a.sh
+++ variables/variables_pass_a.sh
@@ -6,13 +6,17 @@ myvar="shell scripting"
 echo $myvar
 myvar2="TEST 2"
 myvar3="TEST 3"
+myvar4="TEST 4"
 
 echo $myvar2
 echo $myvar3
+echo $myvar4
+
 
 #if you comment the line below, the variable will not get passed into the next script
 export myvar2
 export myvar3
+export myvar4
 
 
 bash /mnt/c/Users/Evan/Desktop/development/bash_scripts/variables/variables_pass_b.sh
